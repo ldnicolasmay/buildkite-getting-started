839602cff60e508cf606227011ac552a54b055fb
Nicolas May <nicolas.may@beam.dental>
2022-05-26 09:04:45 -0400

    Tf AWS provider stuff

--- terraform/foo/main.tf
+++ terraform/foo/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = ""
+    session_name = "buildkite_getting_started"
+  }
+}
+
 resource "aws_s3_bucket" "f" {
   bucket = "my-foo-bucket"
 
